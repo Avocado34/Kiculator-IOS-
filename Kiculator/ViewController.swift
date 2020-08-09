0125f6a5aabf1e0ad43978f939335bdf842af320
Avocado34 <avocado34.131@gmail.com>
2020-08-09 15:40:51 +0900

    잘 되나 확인차 커밋

--- Kiculator/ViewController.swift
+++ Kiculator/ViewController.swift
@@ -33,6 +33,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("viewdidloaded loaded")
+        
         setNotificationCenter()
         
         setSavedItemButton()
